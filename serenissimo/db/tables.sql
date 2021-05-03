cee113db777a64d6a61386438953489deb0ac1db
vrde <agranzot@mailbox.org>
2021-05-03 09:51:31 +0200

    Exclude already booked/vaccinated from main loop

--- serenissimo/db/tables.sql
+++ serenissimo/db/tables.sql
@@ -54,5 +54,6 @@ FROM user
   INNER JOIN subscription ON (user.id = subscription.user_id)
   INNER JOIN status ON (status.id = subscription.status_id)
 WHERE subscription.ulss_id IS NOT NULL
+  AND status_id NOT IN ("already_booked", "already_vaccinated")
   AND subscription.fiscal_code IS NOT NULL
   AND subscription.last_check <= CAST(strftime('%s', 'now') AS INT) - status.update_interval
